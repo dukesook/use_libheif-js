727fb68f92d2efc0a7528d1ae461626248f540eb
dukesook <devonsookhoo14@gmail.com>
2024-05-21 15:14:41 -0600

    simplify example again

--- main.js
+++ main.js
@@ -4,6 +4,22 @@ const { HeifDecoder } = libheif();
 const loadButton = document.querySelector('#loadButton');
 const canvas = document.querySelector('canvas');
 
+// Event Listener
+loadButton.addEventListener('change', function (event) {
+  const file = event.target.files[0];
+  processFile(file);
+});
+
+// Process File
+async function processFile(file) {
+  try {
+    const buffer = await readFile(file);
+    await displayFile(buffer);
+  } catch (err) {
+    console.error('failed to convert image:', err);
+  }
+}
+
 // Read File
 const readFile = (file) =>
   new Promise((resolve, reject) => {
@@ -40,15 +56,3 @@ const displayFile = async (buffer) => {
 
   context.putImageData(imageData, 0, 0);
 };
-
-// Event Listener
-loadButton.addEventListener('change', (event) => {
-  const file = event.target.files[0];
-
-  (async () => {
-    const buffer = await readFile(file);
-    await displayFile(buffer);
-  })().catch((err) => {
-    console.error('failed to convert image:', err);
-  });
-});
