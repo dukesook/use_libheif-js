36c8d40e6231fc911915a7133582d76be2fba9ad
dukesook <devonsookhoo14@gmail.com>
2024-05-21 15:04:49 -0600

    Simplfy Example

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import libheif from 'https://cdn.jsdelivr.net/npm/libheif-js@1.17.1/libheif-wasm/libheif-bundle.mjs';
 const { HeifDecoder } = libheif();
 
-const upload = document.querySelector('#upload');
-const download = document.querySelector('#download');
+const loadButton = document.querySelector('#loadButton');
 const canvas = document.querySelector('canvas');
 
+// Read File
 const readFile = (file) =>
   new Promise((resolve, reject) => {
     const reader = new FileReader();
@@ -14,6 +14,7 @@ const readFile = (file) =>
     reader.readAsArrayBuffer(file);
   });
 
+// Display File
 const displayFile = async (buffer) => {
   const decoder = new HeifDecoder();
 
@@ -40,44 +41,13 @@ const displayFile = async (buffer) => {
   context.putImageData(imageData, 0, 0);
 };
 
-const getCanvasImageBlob = () =>
-  new Promise((resolve, reject) => {
-    canvas.toBlob((blob) => {
-      if (blob) {
-        return resolve(blob);
-      }
-
-      return reject(new Error('failed to convert the image'));
-    });
-  });
-
-upload.addEventListener('change', (event) => {
+// Event Listener
+loadButton.addEventListener('change', (event) => {
   const file = event.target.files[0];
-  if (!file) {
-    return;
-  }
 
   (async () => {
     const buffer = await readFile(file);
-
-    console.time('coversion in:');
     await displayFile(buffer);
-    console.timeEnd('coversion in:');
-
-    // download trick, just for fun
-    const blob = await getCanvasImageBlob();
-    download.onclick = () => {
-      const a = document.createElement('a');
-      document.body.appendChild(a);
-      a.style = 'display: none';
-      const url = window.URL.createObjectURL(blob);
-      a.href = url;
-      a.download = 'result.jpg';
-      a.click();
-      window.URL.revokeObjectURL(url);
-      a.remove();
-    };
-    download.disabled = false;
   })().catch((err) => {
     console.error('failed to convert image:', err);
   });
